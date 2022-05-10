45c6418ce5331cee5af3bf317af3e544371d4b6a
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-05-10 17:14:27 +0800

    update

---
+++ examples/job.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peterzhang
+ * Date: 2022/5/10
+ * Time: 5:13 PM
+ */
+define("ROOT", dirname(__DIR__));
+// DEBUG should create dir use command sudo mkdir /var/log/pumpkinjob && sudo chmod 777 /var/log/pumpkinjob
+define("DEBUG", 1);
+
+function autoload($clazz) {
+    $file = str_replace('\\', '/', $clazz);
+    if(is_file(ROOT . "/pumpkin-job/$file.php")) {
+        require ROOT . "/pumpkin-job/$file.php";
+    }
+}
+
+spl_autoload_register("autoload");
+
+require ROOT . '/vendor/autoload.php';
+
+$domain = "pumpkin-job1.7otech.com";
+$appName = "pumpkinjob-agent-test";
+$password = "pumpkinjob-agent-test";
+
+use IO\Github\PumpkinJob\PumpkinJobClient;
+use IO\Github\PumpkinJob\Enums\TimeExpressionType;
+use IO\Github\PumpkinJob\Enums\ExecuteType;
+use IO\Github\PumpkinJob\Enums\ProcessorType;
+
+$pumpkinJobClient = new PumpkinJobClient($domain, $appName, $password);
+
+$jobId = 2;
+$params = array(
+    "id" => $jobId,
+    "jobName" => "OpenAPIJob",
+    "jobDescription" => "test OpenAPI",
+    "jobParams" => "zdap",
+    "timeExpressionType" => TimeExpressionType::$TYPE[TimeExpressionType::$API],
+    "executeType" => ExecuteType::$TYPE[ExecuteType::$STANDALONE],
+    "processorType" => ProcessorType::$TYPE[ProcessorType::$BUILT_IN],
+    "processorInfo" => "com.yunqiic.pumpkinjob.official.processors.impl.script.ShellProcessor",
+    "designatedWorkers" => "",
+    "minCpuCores" => 1.1,
+    "minMemorySpace" => 1.2,
+    "minDiskSpace" => 1.3,
+
+);
+$result = $pumpkinJobClient->saveJob($params);
